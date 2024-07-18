816cdfa075f5d59128ce0f3471f28b7c28d72ce2
igor.gralak <igor.gralak@accenture.com>
2024-07-18 13:51:45 +0200

    updated invoice entity and dto

--- src/entitities/invoice.entity.ts
+++ src/entitities/invoice.entity.ts
@@ -1,25 +1,98 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Document, Schema as MongooseSchema } from 'mongoose';
+
+@Schema()
+export class Address {
+  @Prop({ required: true })
+  city: string;
+
+  @Prop({ required: true })
+  country: string;
+
+  @Prop({ required: true })
+  line1: string;
+
+  @Prop()
+  line2: string;
+
+  @Prop({ required: true })
+  postal_code: string;
+
+  @Prop({ required: true })
+  state: string;
+}
+
+@Schema()
+export class Consumer {
+  @Prop({ required: true })
+  email: string;
+
+  @Prop({ type: MongooseSchema.Types.Mixed, required: true })
+  address: Address;
+
+  @Prop({ required: true })
+  name: string;
+
+  @Prop()
+  phoneNumber: string;
+}
+
+@Schema()
+export class Company {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  tin_number: string;
+}
+
+@Schema()
+export class LineItem {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  amount_total: number;
+
+  @Prop({ required: true })
+  amount_unit: number;
+
+  @Prop({ required: true })
+  quantity: number;
+
+  @Prop({ required: true, enum: ['pln', 'usd'] })
+  currency: string;
+
+  @Prop({ required: true, enum: ['0%', '20%'] })
+  tax_value: string;
+}
 
 @Schema()
 export class InvoiceEntity extends Document {
   @Prop({ required: true })
-  amount: number;
-
-  @Prop({ required: true })
   date: Date;
 
   @Prop({ required: true })
-  description: string;
-
-  @Prop({ type: String, required: true })
   userId: string;
 
-  @Prop({ type: String, required: true })
-  companyName: string;
+  @Prop({ required: true })
+  finalAmount: number;
+
+  @Prop({ type: MongooseSchema.Types.Mixed, required: true })
+  consumer: Consumer;
+
+  @Prop({ type: MongooseSchema.Types.Mixed, required: true })
+  company: Company;
+
+  @Prop({ type: [MongooseSchema.Types.Mixed], required: true })
+  lineItems: LineItem[];
 
   @Prop({ required: true, unique: true })
   invoiceNumber: number;
 }
 
+export const AddressSchema = SchemaFactory.createForClass(Address);
+export const ConsumerSchema = SchemaFactory.createForClass(Consumer);
+export const CompanySchema = SchemaFactory.createForClass(Company);
+export const LineItemSchema = SchemaFactory.createForClass(LineItem);
 export const InvoiceSchema = SchemaFactory.createForClass(InvoiceEntity);
--- src/modules/invoices/dto/create-invoice.dto.ts
+++ src/modules/invoices/dto/create-invoice.dto.ts
@@ -1,15 +1,112 @@
-import { IsString, IsNumber, IsDateString } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsString, IsNumber, IsDateString, ValidateNested, IsNotEmpty, ArrayNotEmpty, IsArray, IsIn, IsObject, IsOptional } from 'class-validator';
+
+const validCurrencies = ["pln", "usd"]
+const validTaxes = ["20%", "0%"]
+
+export class AddressDto {
+  @IsString()
+  @IsNotEmpty()
+  city: string;
+
+  @IsString()
+  @IsNotEmpty()
+  country: string;
+
+  @IsString()
+  @IsNotEmpty()
+  line1: string;
+
+  @IsString()
+  line2: string;
+
+  @IsString()
+  @IsNotEmpty()
+  postal_code: string;
+
+  @IsString()
+  @IsOptional()
+  state?: string;
+}
+
+export class ConsumerDto {
+  @IsString()
+  @IsNotEmpty()
+  email: string;
+
+  @IsNotEmpty()
+  @ValidateNested()
+  @Type(() => AddressDto)
+  address: AddressDto;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  phoneNumber: string | null;
+}
+
+export class CompanyDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  tin_number: string;
+}
+
+export class LineItemsDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  amount_total: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  amount_unit: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  quantity: number;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsIn(validCurrencies)
+  currency: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsIn(validTaxes)
+  tax_value: string;
+}
 
 export class CreateInvoiceDto {
-    @IsNumber()
-    amount: number;
+  @IsDateString()
+  @IsNotEmpty()
+  date: string;
 
-    @IsDateString()
-    date: Date;
+  @IsString()
+  @IsNotEmpty()
+  userId: string;
 
-    @IsString()
-    description: string;
+  @IsNotEmpty()
+  @ValidateNested()
+  @Type(() => ConsumerDto)
+  consumer: ConsumerDto;
 
-    @IsString()
-    userId: string;
+  @IsNotEmpty()
+  @ValidateNested()
+  @Type(() => CompanyDto)
+  company: CompanyDto;
+
+  @IsArray()
+  @ArrayNotEmpty()
+  @ValidateNested({ each: true })
+  @Type(() => LineItemsDto)
+  lineItems: LineItemsDto[];
 }
