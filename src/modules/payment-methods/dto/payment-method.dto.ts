1c7d50e9387df06bed9a2b113ded8ce9bc31a8a9
igor.gralak <igor.gralak@accenture.com>
2024-07-23 13:02:24 +0200

    added payment methood dto

--- src/modules/payment-methods/dto/payment-method.dto.ts
+++ src/modules/payment-methods/dto/payment-method.dto.ts
@@ -1,4 +1,48 @@
 import { PartialType } from "@nestjs/swagger";
+import { Type } from "class-transformer";
+import { IsCreditCard, IsEnum, IsNotEmpty, IsNumber, IsString, Max, MaxLength, Min, MinLength, ValidateNested } from "class-validator";
 
-export class CreatePaymentMethodDto { }
+enum PaymentMethodType {
+    CREDIT_CARD = 'card',
+}
+
+class CardMethodDto {
+    @IsString()
+    @IsNotEmpty()
+    @IsCreditCard()
+    number: string;
+
+    @IsNumber()
+    @IsNotEmpty()
+    @Max(12)
+    @Min(1)
+    exp_month: number;
+
+    @IsNumber()
+    @IsNotEmpty()
+    @Max(99 * 100)
+    @Min(new Date().getFullYear())
+    exp_year: number;
+
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(3)
+    @MinLength(3)
+    cvc: string;
+}
+
+export class CreatePaymentMethodDto {
+
+    @IsEnum(PaymentMethodType)
+    @IsNotEmpty()
+    type: PaymentMethodType;
+
+    @IsNotEmpty()
+    @ValidateNested()
+    @Type(() => CardMethodDto)
+    method: CardMethodDto;
+
+
+}
+
 export class UpdatePaymentMethodDto extends PartialType(CreatePaymentMethodDto) { }
